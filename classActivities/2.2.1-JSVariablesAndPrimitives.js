4715c192a3813dc2dfde487da3e7e680b3d0f5fd
Julian Brittain <julianbrittain@gmail.com>
2021-04-20 19:47:33 -0500

    js variables and prims

---
+++ classActivities/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,31 @@
+// create a constant variable (const)
+const name = 'Julian';
+console.log(name)
+// create a variable that can be reassigned (let)
+let age = 24;
+
+// create three variables and assign them values of different data types (=)
+const lastName = 'Brittain';
+let dayOfMonth = 20;
+let happiness = true;
+
+
+// print the types of two variables that reference two different data types (typeof) // TIP: console.log() prints a value in the console/terminal
+console.log(typeof dayOfMonth);
+console.log(lastName);
+console.log(happiness);
+
+// change the value referenced by a variable (dynamic typing)
+dayOfMonth = 'Tuesday';
+
+// print the type of the variable you just changed (typeof)
+console.log(dayOfMonth);
+// create variables and give them values to complete the sentences that will print
+
+// which variables need to be created?
+
+// what type of data needs to go in each variable
+const middleName = 'Lavelle';
+let numberOfWeeks = 4
+
+console.log ("hello, my name is " + name + "   I have been learning Javascript for" + numberOfWeeks+  )
