13ecce2372e0f75d5421e38d18b957e53d9f6ab1
Julian Brittain <julianbrittain@gmail.com>
2021-04-26 16:19:30 -0500

    loop homework 4/26

---
+++ classActivities/Savy-Homework.js
@@ -0,0 +1,26 @@
+/*let pizzaPlace = "Julio's Pizza";
+
+let numberOfToppings = 3;
+
+console.log(typeof "Julio's Pizza")
+console.log(typeof 3)
+
+console.log(`Julio's Pizza has a famous ${numberOfToppings} topping pizza that is the best in Illinois`)
+
+
+if (numberOfToppings < 10){
+  console.log("Quality not quantity")
+}
+else if (numberOfToppings >= 10){
+  console.log("A whole lot of pizza")
+}
+*/
+
+//BONUS
+
+/*for(let = amountOfToppings = 1; amountOfToppings <= 10; amountOfToppings++){
+  if (amountOfToppings % 2 === 0){
+    console.log(amountOfToppings)
+  }
+}
+*/
